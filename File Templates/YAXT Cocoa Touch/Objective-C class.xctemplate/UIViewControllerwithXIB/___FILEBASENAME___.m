b391c6e45401ddcee1d336c5baa9931f7dfceb1c
tang3w <tang3w@gmail.com>
2013-04-19 10:49:27 +0800

    Modify the comment

--- File Templates/YAXT Cocoa Touch/Objective-C class.xctemplate/UIViewControllerwithXIB/___FILEBASENAME___.m
+++ File Templates/YAXT Cocoa Touch/Objective-C class.xctemplate/UIViewControllerwithXIB/___FILEBASENAME___.m
@@ -2,8 +2,6 @@
 //
 // Copyright (c) ___YEAR___ ___FULLUSERNAME___
 //
-// The MIT License (MIT)
-//
 // Permission is hereby granted, free of charge, to any person
 // obtaining a copy of this software and associated documentation
 // files (the "Software"), to deal in the Software without
